4b0627bd04ad9f2aff4f332a666452ec7dfde19e
nounch <nounch@outlook.com>
2014-07-25 21:36:39 -0500

    Make `Remove all' and `Move' button icons only
    `Move' next to `Remove all' does not make too much sense anyway.
    Also, with less text, the top bar can be a bit slimmer.

--- examples/example.js
+++ examples/example.js
@@ -12,18 +12,19 @@ $(document).ready(function() {
   hermes.message("That's it! Hope you like it.", {
     'text-align': 'center',
     'font-weight': 'bold',
-  });
-  hermes.message('All message can be <b>scrolled back</b>, in case there are too many.');
-  hermes.message('Did you notice the <span style="background-color: #57534A; color: #F1F1E1; font-size: 12px; border-radius: 5px 5px 0 0; text-shadow: none; padding-left: 10px; padding-right: 10px;">Remove all</span> button above? Also, there are close buttons →');
-  hermes.message('A message can contain HTML, e.g. a <a href="#">link</a>.');
-  hermes.inverse('This message is <b>inverse</b>.');
-  hermes.error('This is an <b>error</b> message.');
-  hermes.warning('This is a <b>warning</b> message.');
-  hermes.info('This is an <b>info</b> message.');
-  hermes.success('This is a <b>success</b> message.');
+  }, false);
+  hermes.message('All message can be <b>scrolled back</b>, in case there are too many.', {}, false);
+  hermes.message('Did you notice the close button (<span style="background-color: #57534A; color: #F1F1E1; font-size: 19px; border-radius: 0 5px 0 0; text-shadow: none; padding-left: 80px; padding-right: 10px;">&times;</span>) above? Also, there →', {}, false);
+  hermes.message('A message can contain HTML, e.g. a <a href="#">link</a>.', {}, false);
+
+  hermes.inverse('This message is <b>inverse</b>.', false);
+  hermes.error('This is an <b>error</b> message.', false);
+  hermes.warning('This is a <b>warning</b> message.', false);
+  hermes.info('This is an <b>info</b> message.', false);
+  hermes.success('This is a <b>success</b> message.', false);
 
   // Source: http://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png
-  hermes.message('Here is an image: <img src="http://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png" style="width: 60px; height: 60px; margin-left: 40px;" alt="No image found" />');
+  hermes.message('Here is an image: <img src="http://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png" style="width: 60px; height: 60px; margin-left: 40px;" alt="No image found" />', {}, false);
 
   // Source: http://www.tizag.com/pics/cssT/smallPic.jpg
   hermes.message('Messages can be styled with CSS.', {
@@ -31,9 +32,9 @@ $(document).ready(function() {
     'color': '#FFFF8A',
     'text-shadow': '0 2px 3px #FF0000, 0 -2px 3px #121212',
     'font-weight': 'bold',
-  });
+  }, false);
 
-  hermes.message('This is a slightly longer message that demonstrates that a message element automatically adjusts to the message length.');
-  hermes.message(testMessage);
+  hermes.message('This is a slightly longer message that demonstrates that a message element automatically adjusts to the message length.', {}, false);
+  hermes.message(testMessage, {}, false);
 
 });
